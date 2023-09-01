e2a3845ca0d0d92c5351dc532664a2a3eda92934
Andrea Riccardi <andrea.riccardi03@gmail.com>
2023-09-01 18:07:50 +0200

    update pub

--- lib/screens/registration_page.dart
+++ lib/screens/registration_page.dart
@@ -1,3 +1,6 @@
+// ignore_for_file: empty_catches, unused_catch_clause
+
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:spotify_copy/services/auth.dart';
