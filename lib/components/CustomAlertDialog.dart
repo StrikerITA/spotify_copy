08438ad3f895c790665b798ce1b6921ed1640d38
Andrea Riccardi <andrea.riccardi03@gmail.com>
2023-09-01 17:28:02 +0200

    add CustomAlertDialog sample

---
+++ lib/components/CustomAlertDialog.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+
+class CustomAlertDialog extends StatefulWidget {
+  String title;
+  String? content;
+  List<Widget> actions;
+
+  CustomAlertDialog({
+    required this.title,
+    this.content = '',
+    required this.actions,
+    Key? key,
+  }) : super(key: key);
+
+  _CustomAlertDialogState createState() => _CustomAlertDialogState();
+}
+
+class _CustomAlertDialogState extends State<CustomAlertDialog> {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoAlertDialog(
+      title: Text(widget.title),
+      content: Text(widget.content ?? ""),
+      //azioni del bottone
+      actions: widget.actions,
+    );
+  }
+}
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:spotify_copy/services/auth.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -10,6 +11,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          IconButton(
+            onPressed: () {
+              Auth().signOut();
+            },
+            icon: Icon(Icons.exit_to_app),
+          ),
+        ],
+      ),
+    );
   }
 }
